bb3e7b7d92ea0d7fae314c1dab43a0e3e5df8874
Nicat Rzayev <nijat.rzayev.aze@gmail.com>
2026-04-17 17:52:05 +0400

    Update 0-square_matrix_simple.py

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
 
 
-matrix = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
-]
+matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
 def square_matrix_simple(matrix=[]):
     return [[x**2 for x in row] for row in matrix]
