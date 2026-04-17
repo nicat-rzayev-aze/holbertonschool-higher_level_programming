0d651b2b075eb59d8425763c1bab4db2f00fc4eb
Nicat Rzayev <nijat.rzayev.aze@gmail.com>
2026-04-17 17:46:03 +0400

    Create 0-square_matrix_simple.py

---
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+def square_matrix_simple(matrix=[]):
+    return [[x**2 for x in row] for row in matrix]
+new_matrix = square_matrix_simple(matrix)
+print(new_matrix)
+print(matrix)
