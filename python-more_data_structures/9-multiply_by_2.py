ddf7d2455b1bfcbd2973bbffb17527dddb6d36f0
Nicat Rzayev <nijat.rzayev.aze@gmail.com>
2026-04-25 18:31:12 +0400

    Update 9-multiply_by_2.py

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -5,4 +5,4 @@ def multiply_by_2(a_dictionary):
     b_dictionary = {}
     for key, value in a_dictionary.items():
         new_dict[key] = value * 2
-    return a_dictionary, b_dictionary
+    return b_dictionary
