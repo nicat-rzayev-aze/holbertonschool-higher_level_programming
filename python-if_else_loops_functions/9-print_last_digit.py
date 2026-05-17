17d2173f1a0f42a223ec77eb5b9e52abc0795694
Nicat Rzayev <nijat.rzayev.aze@gmail.com>
2026-05-17 14:45:52 +0400

    Update 9-print_last_digit.py

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    if number != 0:
-        print(abs(number) % 10, end = "")
-    else:
-        print("0", end = "")
     return print(abs(number) % 10, end = "")
