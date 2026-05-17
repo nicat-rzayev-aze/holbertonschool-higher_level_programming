3c2dc8cb030742019c3f57f88201949c79c6daa1
Nicat Rzayev <nijat.rzayev.aze@gmail.com>
2026-05-17 14:07:31 +0400

    Create 8-uppercase.py

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,8 @@
+def uppercase(str):
+    biggy = ""
+    for c in str:
+        if ord('a') <= ord(c) <= ord('z'):
+            biggy += chr(ord(c) - 32)
+        else:
+            result += c
+    print(result)
