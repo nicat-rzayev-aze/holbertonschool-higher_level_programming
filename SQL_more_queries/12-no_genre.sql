576b6ee70ce4f7fdda265321283fb703f3609da9
Nicat Rzayev <nijat.rzayev.aze@gmail.com>
2026-06-03 19:18:07 +0400

    Create 12-no_genre.sql

---
+++ SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,6 @@
+-- tabletabletable
+SELECT tv_shows.title, tv_show_genres.genre_id 
+FROM tv_shows 
+RIGHT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id 
+WHERE tv_shows.id IS NULL 
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC
