f1366370957c2919ce783f704a78394d76799ffe
Nicat Rzayev <nijat.rzayev.aze@gmail.com>
2026-06-06 16:45:03 +0400

    Update 7-base_geometry.py

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -8,15 +8,7 @@ class BaseGeometry:
     """A class representing base geometry."""
     
     def area(self):
-        """Raise an Exception indicating that area is not implemented.
-        
-        Examples:
-            >>> bg = BaseGeometry()
-            >>> bg.area()
-            Traceback (most recent call last):
-            ...
-            Exception: area() is not implemented
-        """
+        """Raise an Exception indicating that area is not implemented."""
         raise Exception("area() is not implemented")
     
     def integer_validator(self, name, value):
@@ -29,27 +21,6 @@ class BaseGeometry:
         Raises:
             TypeError: If value is not an integer.
             ValueError: If value is less than or equal to 0.
-        
-        Examples:
-            >>> bg = BaseGeometry()
-            >>> bg.integer_validator("my_int", 12)
-            
-            >>> bg.integer_validator("width", 89)
-            
-            >>> bg.integer_validator("name", "John")
-            Traceback (most recent call last):
-            ...
-            TypeError: name must be an integer
-            
-            >>> bg.integer_validator("age", 0)
-            Traceback (most recent call last):
-            ...
-            ValueError: age must be greater than 0
-            
-            >>> bg.integer_validator("distance", -4)
-            Traceback (most recent call last):
-            ...
-            ValueError: distance must be greater than 0
         """
         if type(value) is not int:
             raise TypeError("{} must be an integer".format(name))
