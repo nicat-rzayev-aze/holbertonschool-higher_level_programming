5cd864f57907ae99021c18a07753583687eb03b3
Nicat Rzayev <nijat.rzayev.aze@gmail.com>
2026-06-06 16:50:40 +0400

    Update 7-base_geometry.py

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -4,18 +4,18 @@
 
 class BaseGeometry:
     """A class representing base geometry."""
-    
+  
     def area(self):
         """Raise an Exception indicating that area is not implemented."""
         raise Exception("area() is not implemented")
-    
+
     def integer_validator(self, name, value):
         """Validate that a value is a positive integer.
-        
+
         Args:
             name (str): The name associated with the value.
             value (int): The value to validate.
-            
+
         Raises:
             TypeError: If value is not an integer.
             ValueError: If value is less than or equal to 0.
@@ -25,13 +25,12 @@ class BaseGeometry:
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
 
-
 class Rectangle(BaseGeometry):
     """A class representing a rectangle."""
-    
+
     def __init__(self, width, height):
         """Initialize a Rectangle.
-        
+
         Args:
             width (int): The width of the rectangle.
             height (int): The height of the rectangle.
@@ -40,33 +39,33 @@ class Rectangle(BaseGeometry):
         self.integer_validator("height", height)
         self.__width = width
         self.__height = height
-    
+
     @property
     def width(self):
         """Retrieve the width."""
         return self.__width
-    
+
     @width.setter
     def width(self, value):
         """Set the width."""
         self.integer_validator("width", value)
         self.__width = value
-    
+
     @property
     def height(self):
         """Retrieve the height."""
         return self.__height
-    
+
     @height.setter
     def height(self, value):
         """Set the height."""
         self.integer_validator("height", value)
         self.__height = value
-    
+
     def area(self):
         """Return the area of the rectangle."""
         return self.__width * self.__height
-    
+
     def __str__(self):
         """Return a string representation of the rectangle."""
         return "[Rectangle] {}/{}".format(self.__width, self.__height)
