899fc31c72cbae718c4a5c071f8706c3d990232a
Nicat Rzayev <nijat.rzayev.aze@gmail.com>
2026-06-06 15:55:42 +0400

    Update 3-say_my_name.py

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -4,13 +4,13 @@ This module provides a function `say_name` that prints a formatted name.
 """
 
 
-def say_my_name(first_name, last_name):
+def say_my_name(first_name, last_name=""):
     """
     Prints "My name is <first_name> <last_name>".
     
     Args:
         first_name: The first name (must be a string)
-        last_name: The last name (must be a string)
+        last_name: The last name (must be a string, optional)
         
     Raises:
         TypeError: If first_name or last_name is not a string
@@ -21,4 +21,3 @@ def say_my_name(first_name, last_name):
         raise TypeError("first_name must be a string or last_name must be a string")
     
     print("My name is {} {}".format(first_name, last_name))
-    
