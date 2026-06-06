27ac1f68e91aaa286b3488461e569ab4ad1b5eea
Nicat Rzayev <nijat.rzayev.aze@gmail.com>
2026-06-06 21:51:49 +0400

    Update 1-write_file.py

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-"""For Reading and printing a text file."""
+"""Writes text to a file."""
 
 
-def read_file(filename=""):
-    """Reads a text file and prints it to stdout."""
-    with open(filename, "r", encoding="utf-8") as f:
-        print(f.read(), end="")
+def write_file(filename="", text=""):
+    """Writes a string to a text file and returns character count."""
+    with open(filename, "w", encoding="utf-8") as f:
+        return f.write(text)
