5ba4d393e7bf54ff373280ce1424950f0a4618d1
Nicat Rzayev <nijat.rzayev.aze@gmail.com>
2026-06-06 14:10:46 +0400

    Update 12-fizzbuzz.py

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 def fizzbuzz():
     for i in range(1, 101):
-        if i % 3 == 0 and i % 4 == 0:
-            print("FizzBuzz", end="")
-        elif i % 3 == 0:
-            print("Fizz", end="")
-        elif i % 5 == 0:
-            print("Value", end="")
-
+        output = ""
+        
+        if i % 3 == 0:
+            output += "Fizz"
+        if i % 5 == 0:
+            output += "Buzz"
+        if output == "":
+            output = str(i)
+        
+        print(output, end=" ")
