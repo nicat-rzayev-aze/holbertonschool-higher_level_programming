8b40125e1f9d758891995ae571596d1ba61e1683
Nicat Rzayev <nijat.rzayev.aze@gmail.com>
2026-06-06 14:15:26 +0400

    Update 6-print_comb3.py

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,10 +1,10 @@
-#chmod u+x 6-print_comb3.py
 #!/usr/bin/python3
 
 
 for i in range(10):
     for j in range(i + 1, 10):
-        if i == 8 and j == 9:
-            print("{}{}".format(i, j))
+        print(i, j, sep="", end="")
+        if i != 8 or j != 9:
+            print(", ", end="")
         else:
-            print("{}{}".format(i, j), end=", ")
+            print()
