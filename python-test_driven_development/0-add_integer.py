464b2aa1b6e120c7bd3ac2db26cc420ed9ab06cb
Nicat Rzayev <nijat.rzayev.aze@gmail.com>
2026-06-06 15:34:03 +0400

    Update 0-add_integer.py

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -2,12 +2,15 @@
 """
 I created a function for adding integer
 """
+
+
 def add_integer(a, b=98):
-    """Adds 2 integers."""
+    """
+    Adds 2 integers.
+    """
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
-        
     if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
-        
+
     return int(a) + int(b)
