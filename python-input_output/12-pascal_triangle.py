7a37b462bca7facc3491197ca7e97080e0126067
Nicat Rzayev <nijat.rzayev.aze@gmail.com>
2026-06-09 14:21:30 +0400

    Update 12-pascal_triangle.py

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -12,7 +12,7 @@ def pascal_triangle(n):
     while len(triangle) < n:
         prev_row = triangle[-1]
         new_row = [1]
-    
+
         for i in range(len(prev_row) - 1):
             new_row.append(prev_row[i] + prev_row[i + 1])
 
