1f5de3fb600a795b309d175355c14acb682c9139
Nicat Rzayev <nijat.rzayev.aze@gmail.com>
2026-06-09 13:05:59 +0400

    Update 10-student.py

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -4,13 +4,13 @@
 
 class Student:
     """A class that defines a student"""
-    
+
     def __init__(self, first_name, last_name, age):
         """Initialize a Student."""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
-    
+
     def to_json(self, attrs=None):
         """Return a dictionary representation of a student."""
         if attrs is None:
