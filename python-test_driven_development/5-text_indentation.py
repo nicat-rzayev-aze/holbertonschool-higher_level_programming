0c8bb1032c2761623410372dcb1395bbf50fb93e
Nicat Rzayev <nijat.rzayev.aze@gmail.com>
2026-06-11 19:36:07 +0400

    Update 5-text_indentation.py

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -6,14 +6,4 @@ is exactly an instance of a specified class.
 
 
 def is_same_class(obj, a_class):
-    """
-    Checks if an object is exactly an instance of a specified class.
-
-    Args:
-        obj: The object to check.
-        a_class: The class to match against.
-
-    Returns:
-        True if obj is exactly an instance of a_class, otherwise False.
-    """
     return type(obj) is a_class
