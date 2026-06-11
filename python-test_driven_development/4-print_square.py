6f2889f64753dbc55d1778c4990293d7183365a7
Nicat Rzayev <nijat.rzayev.aze@gmail.com>
2026-06-11 19:23:45 +0400

    Create 4-print_square.py

---
+++ python-test_driven_development/4-print_square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Its function for printing a square with the character #"""
+
+
+def print_square(size):
+    """It will print a square."""
+    if not isinstance(size, int) or isinstance(size, bool):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    for i in range(size):
+        print("#" * size)
