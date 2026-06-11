bd16b3f4299902a7923d4f96ce415d2dfbe9faf1
Nicat Rzayev <nijat.rzayev.aze@gmail.com>
2026-06-11 19:31:07 +0400

    Create 5-text_indentation.py

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+This module provides a function `is_same_class`."""
+
+
+def is_same_class(obj, a_class):
+    """
+    Checks if an object is exactly an instance of a specified class.
+    """
+    return type(obj) is a_class
