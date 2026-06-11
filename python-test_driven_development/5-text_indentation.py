b491b8790870746d69fafc61022e1b82bfd4d551
Nicat Rzayev <nijat.rzayev.aze@gmail.com>
2026-06-11 19:35:39 +0400

    Update 5-text_indentation.py

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,7 +1,19 @@
 #!/usr/bin/python3
-"""This module provides a function is_same_class"""
+"""
+This module provides a function `is_same_class` that checks if an object
+is exactly an instance of a specified class.
+"""
 
 
 def is_same_class(obj, a_class):
-    """Checks if an object is exactly an instance of a specified class."""
+    """
+    Checks if an object is exactly an instance of a specified class.
+
+    Args:
+        obj: The object to check.
+        a_class: The class to match against.
+
+    Returns:
+        True if obj is exactly an instance of a_class, otherwise False.
+    """
     return type(obj) is a_class
