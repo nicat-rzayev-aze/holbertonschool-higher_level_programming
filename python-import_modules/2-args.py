df7ea25e1f7a082dc7f7ad97eef6f2dc0269b991
Nicat Rzayev <nijat.rzayev.aze@gmail.com>
2026-06-13 11:59:14 +0400

    Update and rename 2-args.py to python-import_modules/2-args.py

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 import sys
 
+
 if __name__ == "__main__":
     arg_count = len(sys.argv) - 1
 
@@ -13,4 +14,3 @@ if __name__ == "__main__":
 
     for i in range(1, len(sys.argv)):
         print("{}: {}".format(i, sys.argv[i]))
-
