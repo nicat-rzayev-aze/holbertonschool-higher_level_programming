81b6777d0e28ad811eb7637a6c69994fcdaefa0d
Nicat Rzayev <nijat.rzayev.aze@gmail.com>
2026-06-13 11:49:17 +0400

    Update 5-variable_load.py

--- python-import_modules/5-variable_load.py
+++ python-import_modules/5-variable_load.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-Program that imports the variable a from the file variable_load_5.py and prints its value
+Program that imports the variable a from the file 
+variable_load_5.py and prints its value
 """
 
 
