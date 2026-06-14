487f45b867bd0fed254da157cf96aa72d6fbff94
Nicat Rzayev <nijat.rzayev.aze@gmail.com>
2026-06-14 08:58:50 +0400

    Update 0-select_states.py

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,31 +1,18 @@
 #!/usr/bin/python3
 """
-hbtn_0e_0_usa veritabanındaki tüm eyaletleri (states) listeleyen betik.
+Task 0 - Write a script that lists all states from the database hbtn_0e_0_usa
 """
-import MySQLdb
-import sys
 
 if __name__ == "__main__":
-    mysql_username = sys.argv[1]
-    mysql_password = sys.argv[2]
-    db_name = sys.argv[3]
+    import MySQLdb
+    from sys import argv
 
-    db = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=mysql_username,
-        passwd=mysql_password,
-        db=db_name
-    )
-
-    cursor = db.cursor()
-
-    cursor.execute("SELECT * FROM states ORDER BY id ASC")
-
-    rows = cursor.fetchall()
-
-    for row in rows:
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3], charset="utf8")
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id")
+    query_rows = cur.fetchall()
+    for row in query_rows:
         print(row)
-
-    cursor.close()
+    cur.close()
     db.close()
