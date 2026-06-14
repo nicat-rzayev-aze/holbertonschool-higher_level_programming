4ba65de7d3dd4203c4295dab48c7d2c40cec9b0a
Nicat Rzayev <nijat.rzayev.aze@gmail.com>
2026-06-14 11:44:34 +0400

    Create 7-script.js

---
+++ javascript-dom_manipulation/7-script.js
@@ -0,0 +1,15 @@
+const url = 'https://swapi-api.hbtn.io/api/films/?format=json';
+const listMovies = document.querySelector('#list_movies');
+
+fetch(url)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    const movies = data.results;
+    movies.forEach(function (movie) {
+      const listItem = document.createElement('li');
+      listItem.textContent = movie.title;
+      listMovies.appendChild(listItem);
+    });
+  });
